1ea8685af9d35e7efc8f729723d56d023210ea76
Shin-An <siancat0525@gmail.com>
2022-05-17 21:36:17 +0800

    final version

--- guess-num.py
+++ guess-num.py
@@ -3,14 +3,19 @@
 # 猜對的話 印出"終於猜對了!"
 # 猜錯的話 要告訴他 比答案大/小
 # 延伸:印出猜了幾次
+# 延伸:請使用者決定隨機數範圍
 
 import random
+start = input('請決定隨機數字範圍開始值: ')
+end = input('請決定隨機數字範圍結束值: ')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end) # 變數起始結束值由使用者決定
 count = 0 # count為計數
 while True:
 	count = count + 1 # 可寫成 count += 1
-	g = input('請猜數字1~100: ') # g為猜的數字
+	g = input('請猜數字: ') # g為猜的數字
 	g = int(g)
 	if g == r:
 		print('終於猜對了!')
